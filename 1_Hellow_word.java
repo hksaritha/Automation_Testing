010951dfc675690cdabb4d67f287344d1d979bad
hksaritha <115253346+hksaritha@users.noreply.github.com>
2022-10-07 19:43:17 +0530

    Python introduction
    I uploaded python basic files

---
+++ 1_Hellow_word.java
@@ -0,0 +1,22 @@
+/*  To PRINT HELLOW WORD IN JAVA */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/*  To Run script */
+javac Hellow_word.java 
+
+/* The it will create class file HelloWorld.class */
+java HelloWorld.class
+
+/* To PRINT HELLOW WORD IN PYTHON  */
+print("Hellow World")
+
+python filename.py
+
+
+
+
+
